a06f91096fa4c32a1365ae3058a0c52863c017a3
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-09-24 22:11:17 +0600

    chore:testing

--- src/fmnist_ann/data/preprocessing.py
+++ src/fmnist_ann/data/preprocessing.py
@@ -1,1 +1,19 @@
 from torchvision import transforms
+
+
+def get_train_transform():
+    return transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,),(0.5,)),
+        ]
+    )
+
+
+def get_test_transforms():
+    return transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,),(0.5,)),
+        ]
+    )
